a44094d8e6ebfa76b85e09e28c6761b14504e242
Ariel Zapata <ariel.zapata.j@gmail.com>
2019-12-18 17:40:05 -0600

    spec

--- spec/controllers/reports_controller_spec.rb
+++ spec/controllers/reports_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ReportsController, type: :controller do
   describe "controller" do
-    it "Se ha realizado la prueba" do
-      get:info
+    it "the test was successfully" do
+      get :info
       expect(response.status).to eq(200) 
     end
   end
--- spec/routing/reports_routing_spec.rb
+++ spec/routing/reports_routing_spec.rb
@@ -2,7 +2,7 @@ require "rails_helper"
 
 RSpec.describe ReportsController, type: :routing do
   describe "routing" do
-    it "routes hola mundo to reports/info" do
+    it "reponse was succesfully to reports/info" do
       expect(get: "/reports/info").to route_to("reports#info")
     end
   end
